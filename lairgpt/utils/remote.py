42f7debab88a91b22e43947164f8e96989384e1a
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-18 14:25:34 +0000

    Adding tarfile member sanitization to extractall()

--- lairgpt/utils/remote.py
+++ lairgpt/utils/remote.py
@@ -146,7 +146,26 @@ def download_latest(filename):
 
     # extract |> remove archive
     with tarfile.open(dest + ".tar.gz", "r:gz") as tar:
-        tar.extractall(local_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, local_dir)
     os.remove(dest + ".tar.gz")
     print("\nDownloaded at " + dest)
 
